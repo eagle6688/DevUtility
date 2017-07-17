c5af5204088fb55b6ecc9733235763b023b32a91
su liang <fangyansu@163.com>
2017-07-17 17:26:01 +0800

    Update

--- DevUtility.Test/DevUtility.Test.WinForm/DevUtility.Test.WinForm.Service/Database/MongoDBTest/QueryService.cs
+++ DevUtility.Test/DevUtility.Test.WinForm/DevUtility.Test.WinForm.Service/Database/MongoDBTest/QueryService.cs
@@ -1,5 +1,6 @@
 ﻿using DevUtility.Win.Services.AppService;
 using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
 using MongoDB.Driver;
 using System;
 using System.Collections.Generic;
@@ -36,8 +37,9 @@ namespace DevUtility.Test.WinForm.Service.Database.MongoDBTest
             IMongoCollection<BsonDocument> collection = database.GetCollection<BsonDocument>("Profiles");
             var filter = Builders<BsonDocument>.Filter.Eq("User", "1300023121843");
             var fields = Builders<BsonDocument>.Projection.Exclude("_id").Include("User");
-            var result = collection.Find(filter).Project(fields).ToList();
-            DisplayMessage("OK");
+            var result = collection.Find(filter).Project(fields).FirstOrDefault();
+            var dic = BsonSerializer.Deserialize<Dictionary<string, string>>(result);
+            DisplayMessage(dic["User"]);
         }
 
         #endregion
