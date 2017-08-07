c098735e489a13d9b82805c8006b90b47b5a8993
su liang <fangyansu@163.com>
2017-08-07 13:36:20 +0800

    Update jquery.kotable.helper.js

--- DevUtility.Test/DevUtility.Test.WebForm/jquery.plugins/jquery.kotable.helper/jquery.kotable.helper.js
+++ DevUtility.Test/DevUtility.Test.WebForm/jquery.plugins/jquery.kotable.helper/jquery.kotable.helper.js
@@ -1,25 +1,23 @@
 ﻿(function ($, window, document, undefined) {
     var pluginName = 'koTableHelper',
-        version = 'v4.0.20170804';
+        version = 'v4.0.20170807';
 
     var defaults = {
-        url: '',
-        requestType: 'GET',
-        requestData: null,
-        viewModel: null,
-        selectAllBtn: '#cb-selectAll',
-        selectItemsSelector: 'tbody input[type="checkbox"]',
-        pageSize: 10,
-        autoLoad: true,
-        noDataDom: '', //Dom displays when there is no datum loaded.
-        loadingDom: '', //Dom displays when data are loading.
-        beforeLoadData: function (data) { },
-        afterLoadData: function (data) { }
+        ko: {
+            selector: '',
+            options: null
+        },
+        pagination: {
+            selector: '',
+            options: null
+        },
+        mobilepagination: {
+            selector: '',
+            options: null
+        }
     };
 
-    function Plugin(element, options) {
-        this.element = element;
-        this.$element = $(element);
+    function Plugin(options) {
         this.options = $.extend(true, {}, defaults, options);
         this._init();
     }
@@ -27,14 +25,97 @@
     Plugin.prototype.constructor = Plugin;
 
     Plugin.prototype._init = function () {
+        this.$ko = null;
+        this.$pagination = null;
+        this.$mobilepagination = null;
 
+        this._initKo();
+        this._initPagination();
+        this._initMobilePagination();
     };
 
-    $.fn[pluginName] = function (options) {
-        return this.each(function () {
-            if (!$.data(this, pluginName)) {
-                $.data(this, pluginName, new Plugin(this, options));
+    Plugin.prototype._initKo = function () {
+        var self = this,
+            options = {},
+            beforeLoadData = null;
+
+        if (self.options.ko.options && self.options.ko.options.beforeLoadData) {
+            beforeLoadData = self.options.ko.options.beforeLoadData;
+        }
+
+        options.beforeLoadData = function (data) {
+            if (beforeLoadData) {
+                beforeLoadData(data);
             }
-        });
+
+            if (self.$pagination) {
+                self.$pagination.data('pagination').changeTotalRecords(data.Count);
+            }
+
+            if (self.$mobilepagination) {
+                self.$mobilepagination.data('paginationmobile').changeTotalRecords(data.Count);
+            }
+        };
+
+        this.options.ko.options = $.extend(true, {}, this.options.ko.options, options);
+        this.$ko = $(this.options.ko.selector).koHelper(this.options.ko.options);
+    };
+
+    Plugin.prototype._initPagination = function () {
+        var self = this,
+            options = {},
+            onPageClick = null;
+
+        if (self.options.pagination.options && self.options.pagination.options.onPageClick) {
+            onPageClick = self.options.pagination.options.onPageClick;
+        }
+
+        options.onPageClick = function (pageIndex) {
+            if (onPageClick) {
+                onPageClick(pageIndex);
+            }
+
+            if (self.$mobilepagination) {
+                self.$mobilepagination.data('paginationmobile').changePageIndex(pageIndex);
+            }
+
+            if (self.$ko) {
+                self.$ko.data('koHelper').changePage(pageIndex);
+            }
+        };
+
+        this.options.pagination.options = $.extend(true, {}, this.options.pagination.options, options);
+        this.$pagination = $(this.options.pagination.selector).pagination(this.options.pagination.options);
+    };
+
+    Plugin.prototype._initMobilePagination = function () {
+        var self = this,
+            options = {},
+            onPageClick = null;
+
+        if (self.options.mobilepagination.options && self.options.mobilepagination.options.onPageClick) {
+            onPageClick = self.options.mobilepagination.options.onPageClick;
+        }
+
+        options.onPageClick = function (pageIndex) {
+            if (onPageClick) {
+                onPageClick(pageIndex);
+            }
+
+            if (self.$pagination) {
+                self.$pagination.data('pagination').changePageIndex(pageIndex);
+            }
+
+            if (self.$ko) {
+                self.$ko.data('koHelper').changePage(pageIndex);
+            }
+        };
+
+        this.options.mobilepagination.options = $.extend(true, {}, this.options.mobilepagination.options, options);
+        this.$mobilepagination = $(this.options.mobilepagination.selector).paginationmobile(this.options.mobilepagination.options);
+    };
+
+    $[pluginName] = function (options) {
+        return new Plugin(options);
     };
 })(jQuery, window, document);
