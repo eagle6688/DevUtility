a7a94bab01fa58c95df0e4a3a0f479a7c271a07d
su liang <fangyansu@163.com>
2017-09-17 11:36:21 +0800

    Update

---
+++ DevUtility.Com/Test/ITest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DevUtility.Com.Test
+{
+    public interface ITest
+    {
+        #region Methods
+
+        void Start();
+
+        #endregion
+    }
+}
