dd8c37810d4a1cb51d776a54bacd078f99c663d7
su liang <fangyansu@163.com>
2017-09-17 22:18:56 +0800

    Update

---
+++ DevUtility.Test/DevUtility.Test.Common/Winform/TestTypes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DevUtility.Test.Common.Winform
+{
+    public enum TestType
+    {
+        Unkonw = 0,
+        T1,
+        T2,
+        T3
+    }
+}
--- DevUtility.Test/DevUtility.Test.WinForm/DevUtility.Test.WinForm.Service/Data/Enums/GetTypeService.cs
+++ DevUtility.Test/DevUtility.Test.WinForm/DevUtility.Test.WinForm.Service/Data/Enums/GetTypeService.cs
@@ -1,5 +1,6 @@
 ﻿using DevUtility.Com.Data;
 using DevUtility.Out.Net.WCF;
+using DevUtility.Test.Common.Winform;
 using DevUtility.Win.Services.AppService;
 using System;
 using System.Collections.Generic;
@@ -20,8 +21,18 @@ namespace DevUtility.Test.WinForm.Service.Data.Enums
 
         public override void Start()
         {
-            var bindingType = EnumHelper.GetType<BindingType>(type);
-            DisplayMessage(bindingType.ToString());
+            //var bindingType = EnumHelper.GetType<BindingType>(type);
+            //DisplayMessage(bindingType.ToString());
+
+            var testType = EnumHelper.GetType<TestType>(type);
+            DisplayMessage(testType.ToString());
+
+            var testType1 = (TestType)1;
+            DisplayMessage(testType1.ToString());
+
+            var testType2 = Enum.Parse(typeof(TestType), type);
+            DisplayMessage(testType2.ToString());
+            DisplayMessage(((int)testType2).ToString());
         }
     }
 }
